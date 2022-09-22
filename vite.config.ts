19998636be63ddafcf0e7f413bce745c1a7391bd
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2022-09-22 20:07:27 +0200

    added ESLint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-	]
-})
+    vue(),
+    vuetify({ autoImport: true }),
+    eslintPlugin({
+      include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
+    }),
+  ],
+  resolve: {
+    // configure the path alias
+    alias: {
+      "@": "/src",
+    },
+  },
+});
